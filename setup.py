8a45cad33df45575f277d60f97741b4b00276c49
BlazarBeam <156617433+BlazarBeam@users.noreply.github.com>
2024-03-02 15:38:52 -0500

    Updating docs for release
    Still figuring out the .yaml files and setup.py files.

--- setup.py
+++ setup.py
@@ -20,26 +20,6 @@ setup(
     # Find all packages in the project
     packages=find_packages(),
 
-    # Classifiers help users find your project by categorizing it.
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-        
-        # Indicate who your project is intended for
-        'Intended Audience :: Engineers',
-        'Topic :: Instrumentation :: Metrology',
-
-        # Pick your license as you wish
-        'License :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.11',
-    ],
-
     # What does your project relate to?
     keywords='instrumentation control SCPI test measurement plotting real-time data',
 )
